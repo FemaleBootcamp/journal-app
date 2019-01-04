6cac39e97aa5317cf9413e580465cf687cbb5c6f
TamaraStankovska <stankovskatamara23@yahoo.com>
2019-01-04 19:14:31 +0100

    Fixed a typo in the Footer section

--- resources/views/partials/footer-partials.blade.php
+++ resources/views/partials/footer-partials.blade.php
@@ -27,7 +27,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
                 <p style="color: lightgray;"><u>National Transaction Corporation</u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
-                <p class="h6" style="color: lightgray;">&copy All right Reversed.Motus</p>
+                <p class="h6" style="color: lightgray;">&copy All right Reserved.Motus</p>
             </div>
             </hr>
         </div>
