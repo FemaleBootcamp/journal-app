1ab96e30c0c53b490e0c359772883c25d08ec6ae
TamaraStankovska <stankovskatamara23@yahoo.com>
2019-01-19 13:52:36 +0100

    Binded inputs to props of the JournalListCompio

--- resources/js/components/JournalListComponent.vue
+++ resources/js/components/JournalListComponent.vue
@@ -1,23 +1,30 @@
 <template>
     <div>
         <div class="row">
-            <div class="col-lg-4 offset-lg-8">
+            <div class="col-lg-3 d-lg-inline">
                 <form id="filter-journal-form">
                     <input id="user_id" v-model="userid" name="user_id" type="hidden">
-                    <h5 class="mt-5 col-6 text-left text-white" style="alignment: left"><span>Date Range:</span></h5>
+                    <h5 class="mt-5 text-left text-white" style="alignment: left"><span>Date Range:</span></h5>
                     <datepicker name="dateFrom" placeholder="Date From:"
                                 style="margin-bottom: 10px" v-model="dateFrom"></datepicker>
                     <datepicker name="dateTo" placeholder="Date To:" v-model="dateTo"></datepicker>
-                    <h5 class="mt-5 col-lg-12 text-left text-white"><span
-                            style="alignment: left">Status of the Goal:</span></h5>
-                    <input class="form-control mt-2" name="goalStatus" placeholder="Status Goal" type="text"
-                           v-model="goalStatus">
                 </form>
-                <div slot="footer" style="margin-top: 10px; margin-right:-10px; margin-bottom: 60px">
-                    <button @click="filter" class="btn btn-primary modal-default-button mr-2">Apply Filter</button>
-                </div>
             </div>
+            <div class="col-lg-4 offset-1">
+                <h5 class="mt-5 text-left text-white"><span
+                        style="alignment: left">Status of the Goal:</span></h5>
+                <div class="form-group">
+                    <select class="form-control" id="sel1">
+                        <option v-model="goalStatus">Achieved</option>
+                        <option v-model="goalStatus">Not-Achieved</option>
+                    </select>
+                </div>
+                <button @click="filter" class="btn btn-primary modal-default-button mr-2">Apply Filter</button>
+            </div>
+
         </div>
+
+
         <table class="table">
             <thead class="thead-dark">
             <tr>
@@ -33,8 +40,6 @@
             <journal :key="journal.id" v-bind="journal" v-for="journal in journals"></journal>
             </tbody>
         </table>
-
-
     </div>
 </template>
 <script>
@@ -63,10 +68,10 @@
             };
         },
         methods: {
-            read(userid = null, dateFrom = null, dateTo = null, goalStatus = null) {
+            read(dateFrom = null, dateTo = null, goalStatus = null) {
                 window.axios.get('/api/journals', {
                     params: {
-                        userId: userid,
+                        userId: this.userid,
                         dateFrom: dateFrom,
                         dateTo: dateTo,
                         goalStatus: goalStatus,
@@ -82,14 +87,13 @@
             filter() {
                 this.journals = [];
                 this.read(
-                    this.userId,
                     this.dateFrom == null ? null : moment(this.dateFrom).format('YYYY-MM-DD'),
                     this.dateTo == null ? null : moment(this.dateTo).format('YYYY-MM-DD'),
                     this.goalStatus);
             }
         },
         created() {
-            this.read(this.userId);
+            this.read();
         }
     }
 </script>
